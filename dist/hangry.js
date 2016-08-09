a10f020591efcbe02451c629489df7f1931faae4
Sean Ellis <seane235@gmail.com>
2016-08-08 22:47:32 -0400

    add mimosa and coffee

--- dist/hangry.js
+++ dist/hangry.js
@@ -103141,7 +103141,7 @@ var Hangry =
 	      game.load.image('platform', 'assets/platform.png');
 	      game.load.image('diamond', 'assets/diamond.png');
 	      game.load.image('star', 'assets/star.png');
-	      game.load.image('pbr', 'assets/pbr.png');
+	      game.load.image('pbr', 'assets/coffee.png');
 	      game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	    }
 	  }, {
--- src/states/state.boot.js
+++ src/states/state.boot.js
@@ -11,7 +11,7 @@ export default class Boot extends Phaser.State {
     game.load.image('platform', 'assets/platform.png');
     game.load.image('diamond', 'assets/diamond.png');
     game.load.image('star', 'assets/star.png');
-    game.load.image('pbr', 'assets/pbr.png');
+    game.load.image('pbr', 'assets/coffee.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   }
 
